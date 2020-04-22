758f00754eabb964c57d06fa5cf5be5ab47462f4
erwann <erwann.fady@gmail.com>
2020-04-22 22:30:31 +0200

    attente 2e joueur

--- src/Main.java
+++ src/Main.java
@@ -15,11 +15,11 @@ public class Main {
     public static void cretionFichier() {
         fichier = new Fichier(false);
 
+        fichier.start();
         fenetre = new Fenetre(fichier.getGrille(), fichier.getCoups(), fichier.getJoueur(), fichier.getNumero());
         fenetre.pack();
         fenetre.setVisible(true);
         System.out.println(fichier.getCoups());
-        fichier.start();
     }
 
     public static void update() {
